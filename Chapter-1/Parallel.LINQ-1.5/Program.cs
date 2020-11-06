e95a304407b6ff9730207439a939762ded36c37d
Rafael <webdeveloperrm@gmail.com>
2020-11-05 23:25:33 -0300

    capitulo 1

---
+++ Chapter-1/Parallel.LINQ-1.5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public string City { get; set; }
+        };
+
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[]
+            {
+                new Person { Name = "Alan", City = "Hull"},
+                new Person { Name = "Beryl", City = "Seattle"},
+                new Person { Name = "Charles", City = "London"},
+                new Person { Name = "David", City = "Seattle"},
+                new Person { Name = "Eddy", City = "Paris"},
+                new Person { Name = "Fred", City = "Berlin"},
+                new Person { Name = "Gordon", City = "Hull"},
+                new Person { Name = "Herry", City = "Seattle"},
+                new Person { Name = "Isaac", City = "Seattle"},
+                new Person { Name = "Rafael", City = "Fortaleza"},
+            };
+
+            var result = from person in people.AsParallel()
+                         where person.City == "Seattle"
+                         select person;
+
+            foreach (var person in result)            
+                Console.WriteLine(person.Name);
+
+            Console.WriteLine("Finished processing. Press a key to end.");
+            Console.ReadKey();
+            
+        }
+    }
+}
+
